0d78d73f99bca80cf85ce4c1a9aebca29d816a49
幽香乐容 <dbh2858304517@gmail.com>
2025-08-15 12:53:11 +0800

    [code]: 规范代码书写，整理代码 [UI]: 测试UI组件热更新

--- main.lua
+++ main.lua
@@ -4,18 +4,29 @@ local gameDisplay = require("display")
 
 -- 初始化矩形的一些默认值
 function love.load()
-    love.window.setFullscreen(false)
-    local msgpack = require("libs/msgpack")
     -- load background image
     gameDisplay.backgroundImage = love.graphics.newImage("resources/bg.png")
+    love.keyboard.setKeyRepeat(false)
 end
 
 function love.update(dt)
-    UI.update()
+    UI:update()
 end
 
 -- 绘制背景
 function love.draw()
     -- 图片大小自适应，根据Body尺寸显示，保持纵横比, 并居中显示
-    UI.draw()
+    UI:draw()
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.quit()
+    end
+end
+
+function love.keyreleased(key)
+    if key == "a" then
+        UI.body.children.main_menu.is_display = not UI.body.children.main_menu.is_display
+    end
 end
